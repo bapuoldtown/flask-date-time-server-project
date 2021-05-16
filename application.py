3de4c2d79cce08bc180f63f07e8139805d770dc7
bapuoldtown <58926512+bapuoldtown@users.noreply.github.com>
2021-05-16 16:10:51 +0530

    Update application.py

--- application.py
+++ application.py
@@ -16,5 +16,7 @@ def hello_world(name):
 def greet_HelloWorld(name):
     return "<h1>Hello, {} Time now is {} , and Today is {}  </h1>".format(name.title(),datetime.now().strftime("%H:%M"),date.today().strftime("%A %d %B %Y"))
 
+   '''
 if __name__ == '__main__':
    application.run(host='0.0.0.0',debug=False)
+  '''
